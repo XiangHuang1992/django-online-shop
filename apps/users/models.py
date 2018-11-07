634599dd5f15563573fd0657c0e45ac91c836351
huangxiang <huangxiang1992@hotmail.com>
2018-11-07 10:56:50 +0800

    创建model完成

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 
 from django.contrib.auth.models import AbstractUser
@@ -10,6 +12,27 @@ class UserProfile(AbstractUser):
     """
     name = models.CharField(max_length=20, null=True, blank=True, verbose_name='用户名')
     birthday = models.DateField(null=True, blank=True, verbose_name='出生年月')
-    mobile = models.CharField()
-    gender = models.CharField(max_length=6, choices=(('male', '男'), ('female', '女')), verbose_name='性别')
+    mobile = models.CharField(max_length=11, verbose_name='手机号码')
+    gender = models.CharField(max_length=6, choices=(('male', '男'), ('female', '女')), verbose_name='性别', default='male')
     email = models.EmailField(max_length=80, null=True, blank=True, verbose_name='邮箱')
+
+    class Meta:
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class VerifyCode(models.Model):
+    code = models.CharField(max_length=10, verbose_name='验证码')
+    mobile = models.CharField(max_length=11, verbose_name='手机号码')
+    add_time = models.CharField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '验证码'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.code
+
