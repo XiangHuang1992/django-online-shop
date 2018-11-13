9079f6337b0f19c0990d5bc993df58ea35ace0c3
huangxiang <huangxiang1992@hotmail.com>
2018-11-13 13:35:44 +0800

    使用django filters进行过滤

---
+++ apps/goods/filters.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+"""
+@version: 0.1
+@author: ferdinand
+@email: ferinandhx@gmail.com
+@flie: filters.py
+@ide: PyCharm
+@time: 2018/11/13 13:35
+"""
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -4,6 +4,7 @@
 # from rest_framework.response import Response
 from rest_framework import mixins
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 # from rest_framework import generics
 # from django.http import Http404
 
@@ -19,10 +20,5 @@ class GoodsViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = Goods.objects.all()
     serializer_class = GoodsSerializer
     pagination_class = GoodListPagination
-
-    def get_queryset(self):
-        queryset = Goods.objects.all()
-        price_min = self.request.query_params.get("price_min", 0)
-        if price_min:
-            queryset = queryset.filter(shop_price__gt=int(price_min))
-        return queryset
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('category',)
